29d9dd04f48deabe220826195d25579ab0b60597
Axel Ridray <axel.ridray@sfr.fr>
2021-07-17 16:52:11 +0200

    mise en place vueX

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,28 @@
 import { createStore } from 'vuex'
 
+//create new store instance
+const store = createstore({
+  state: {
+    currentText: {},
+    texts: [{
+      id:1,
+      title: "Présentation",
+      image: require(),
+      text:"Bonjour, je m' appelle Axel j'ai 26 ans et en 2021 en période de pandémie j'ai pris conscience que le métier que j'exercer depuis mes 17 ans ne m'épanouissais plus et de fil en aiguille j'en suis venu a me reconvertir.",
+    },{
+      id: 1,
+      title: "La Restauration",
+      image: require(),
+      text:"",
+    }, {
+      id: 1,
+      title: "Le Développement Web",
+      image: require(),
+      text:"",
+    }]
+  }
+})
+
 export default createStore({
   state: {
   },
---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1 class="title">hello 404</h1>
+</template>
+
+<style scoped>
+ .title{
+   color: white,
+ }
+</style>
